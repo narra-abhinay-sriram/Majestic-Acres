71912ff5a96c05eb4902e6dd0cd84fd1d5825e5e
narra-abhinay-sriram <121sriramreddy@gmail.com>
2024-10-07 13:15:27 +0530

    db connected

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,14 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+mongoose.connect(process.env.mongo).then(()=>{
+    console.log("db is connected")
+}).catch((e)=>{
+    console.log(e)
+
+})
+
 const app=express()
 
 app.listen(3000,()=>{console.log("app running at 3000  ")})
