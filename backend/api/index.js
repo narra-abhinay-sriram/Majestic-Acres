a1af2e6f36de7d645259d36e30b1a32df7e4a51a
narra-abhinay-sriram <121sriramreddy@gmail.com>
2024-10-12 01:54:17 +0530

    add preinstallation

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,9 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import path from 'path';
+const __dirname = path.resolve();
+
 const app=express()
 //app.use(cors())
 app.use(cors({
@@ -16,3 +19,8 @@ import mainRouter from "../routes/index.js"
 app.use("/api/v1",mainRouter)
 
 app.listen(3000,()=>{console.log("app running at 3000  ")})
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
